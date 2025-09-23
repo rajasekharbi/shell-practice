4171c6e455c6265cbeb41a95252ea27edbb352fc
rajasekhar burri <burriraja@gmail.com>
2025-09-23 22:21:03 +0530

    general coversation

--- 08-variable.sh
+++ 08-variable.sh
@@ -7,3 +7,11 @@ sleep 10
 END_TIME=$(($END_TIME-$START_TIME))
 
 echo "Script excuted in : $TOTAL_TIME Seconds"
+
+
+
+
+#100.27.222.8 | 172.31.24.42 | t3.micro | https://github.com/rajasekharbi/shell-practice.git
+#[ ec2-user@ip-172-31-24-42 ~/shell-practice ]$ sh 08-variable.sh
+#Script excuted in :  Seconds
+
