97eac89514410527eede3e5b2343e286872a8401
rajasekhar burri <burriraja@gmail.com>
2025-09-23 17:54:46 +0530

    general coversation

--- 05-Converstion.sh
+++ 05-Converstion.sh
@@ -1,6 +1,9 @@
 #1/bin/badh
 
 echo "please enter your pin number"
-read pin_number # here pin number is variable
+read -s pin_number # here pin number is variable 
+
+ #-s is not show the command
+
 
 echo "pin entered is : $pin_number"
