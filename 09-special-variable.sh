1b5b2d55c4aa8ee1b4c498afbef915974ea6bf18
rajasekhar burri <burriraja@gmail.com>
2025-09-23 22:17:27 +0530

    general coversation

--- 09-special-variable.sh
+++ 09-special-variable.sh
@@ -9,3 +9,15 @@ echo "Home directory of user: $HOME"
 echo "PID of this script: $$" 
 sleep 50 &
 echo "pin of lass command in background: $!"
+
+
+#100.27.222.8 | 172.31.24.42 | t3.micro | https://github.com/rajasekharbi/shell-practice.git
+#[ ec2-user@ip-172-31-24-42 ~/shell-practice ]$ sh 09-special-variable.sh Rajasekhar modi pk
+#ALL variables passed to the script: Rajasekhar modi pk
+#ALL variables passed to the script: Rajasekhar modi pk
+#script name: 09-special-variable.sh
+#current directory: /home/ec2-user/shell-practice
+#who is running this: ec2-user
+#Home directory of user: /home/ec2-user
+#PID of this script: 2828
+#pin of lass command in background: 2829
