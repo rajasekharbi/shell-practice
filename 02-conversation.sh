981010a6d1680b83587491c82c6c130fdadb83cb
rajasekhar burri <burriraja@gmail.com>
2025-09-23 17:11:43 +0530

    some message

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: hi suresh, how are you?"
+echo "suresh:: hello Ramesh, iam fine.how are you?"
+echo "ramesh:: what are you learing recently?"
+echo "suresh:: i am learing shell scrpting Rames"
