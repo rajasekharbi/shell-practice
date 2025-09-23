04d7deb9b519bd0d08f1302e2bcf9ef4a3282ad3
rajasekhar burri <burriraja@gmail.com>
2025-09-23 17:46:14 +0530

    some message

---
+++ 04-conversation.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+person1=JOHN
+person2=$person2
+
+
+echo "$person1:: hi $person1, how are you?"
+echo "$person1:: hello $person2, iam fine.how are you?"
+echo "$person2:: what are you learing recently?"
+echo "$person1:: i am learing shell scrpting $person2"
