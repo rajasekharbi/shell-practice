71be11b92eeb3262b8b2af3232b19f89e6834d37
rajasekhar burri <burriraja@gmail.com>
2025-09-23 23:44:32 +0530

    general coversation

--- 11-Conditions.sh
+++ 11-Conditions.sh
@@ -9,3 +9,8 @@ elif  [ $NUMBER -eq 10 ]; then
 else
     echo "Given number $NUMBER is greater than 10"
 fi
+
+#100.27.222.8 | 172.31.24.42 | t3.micro | https://github.com/rajasekharbi/shell-practice.git
+#[ ec2-user@ip-172-31-24-42 ~/shell-practice ]$ sh 11-Conditions.sh 55
+#Given number 55 is greater than 10
+
