258edf7d56195d2bcd045294eaf7e9d2c944aedd
rajasekhar burri <burriraja@gmail.com>
2025-09-24 00:43:03 +0530

    general coversation

--- 13-condtions_prime.sh
+++ 13-condtions_prime.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 num=$1
-if [ $num -le 1 ]; then 
+
+if [ $num -le 1 ]; 
+  then 
   echo "Not Prime"; exit; 
   fi
 for (( i=2; i*i<=num; i++ ));
    do
- if (( num % i == 0 )); then echo "Not Prime"; exit; fi
+ if (( num % i == 0 )); 
+    then echo "Not Prime"; 
+   exit; 
+  fi
 done
+
 echo "Prime"
