57144a1f71e8cdee8910555034314c1cb54747b7
rajasekhar burri <burriraja@gmail.com>
2025-09-23 23:36:13 +0530

    general coversation

--- 11-Conditions.sh
+++ 11-Conditions.sh
@@ -9,3 +9,12 @@ else
 fi
 
 
+NUMBER=$1
+
+if [ $NUMBER -lt 10 ]; then
+    echo "Given number $NUMBER is less than 10"
+elif if [ $NUMBER -ql 10 ]; then
+    echo "Given number $NUMBER is equal to 10"
+else
+    echo "Given number $NUMBER is greater than or equal to 10"
+fi
