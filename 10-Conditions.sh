157db204614be4d9a369f1e8f15d1163e228a5a0
rajasekhar burri <burriraja@gmail.com>
2025-09-23 23:30:30 +0530

    general coversation

---
+++ 10-Conditions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+NUMBER =$1
+
+if [$NUMBERn -lt 10]; then
+    echo "Given number $NUMBER is less than 10"
+else
+    echo "Given number $NUMBER is greater than or equal to 10"
+fi
+
+
