fc2f59750d296675c96dbae8a925035b7442d1dc
rajasekhar burri <burriraja@gmail.com>
2025-09-23 23:54:57 +0530

    general coversation

--- 12-condition.sh
+++ 12-condition.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "please enter the number :"
+echo "please enter the number:"
 read NUMBER
 
-if [$(($NUMBER % 2)) -eq 0 ]; then
+if [ $(($NUMBER % 2)) -eq 0 ]; then
     echo "Given Number $Number is EVEN"
 else
     echo "Given Number $NUMBER is ODD"
