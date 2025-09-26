50c6487c975ee4b802f97c88aa49c4a9e9a714dd
rajasekhar burri <burriraja@gmail.com>
2025-09-26 21:50:24 +0530

    general coversation

--- 08-special-variable.sh
+++ 08-special-variable.sh
@@ -2,13 +2,13 @@
 
 echo "ALL variables passed to the script: $@"
 echo "ALL variables passed to the script: $*"
-echo "script name: $0"
-echo "current directory: $PWD"
-echo "who is running this: $USER"
-echo "Home directory of user: $HOME"
-echo "PID of this script: $$" 
-sleep 50 &
-echo "pin of lass command in background: $!"
+#echo "script name: $0"
+#echo "current directory: $PWD"
+#echo "who is running this: $USER"
+#echo "Home directory of user: $HOME"
+#echo "PID of this script: $$" 
+#sleep 50 &
+#echo "pin of lass command in background: $!"
 
 #result
 
