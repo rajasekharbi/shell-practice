08bbf5ad0de95dd8ccf9dd68e5437491c72b3699
rajasekhar burri <burriraja@gmail.com>
2025-09-26 21:30:25 +0530

    general coversation

--- 07-variable.sh
+++ 07-variable.sh
@@ -2,7 +2,7 @@
 
 DATE=$(date)
 
-echo "timestamp executed:$DATE"
+echo "timestamp executed: $DATE"
 
 # 100.27.222.8 | 172.31.24.42 | t3.micro | https://github.com/rajasekharbi/shell-practice.git
 #[ ec2-user@ip-172-31-24-42 ~/shell-practice ]$ sh 07-variable.sh
