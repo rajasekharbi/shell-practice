0423ef39cbd5688c3a9b21bb9918930fd8454cbb
rajasekhar burri <burriraja@gmail.com>
2025-09-26 21:47:25 +0530

    general coversation

--- 08-variable.sh
+++ 08-variable.sh
@@ -2,11 +2,11 @@
 
 START_TIME=$(date +%s)
 
-sleep 5
+
 
 END_TIME=$(date +%s)
 
-TOTAL_TIME=$(($END_TIME-$END_TIME))
+TOTAL_TIME=$(($END_TIME-$START_TIME))
 
 echo "Script excuted in : $TOTAL_TIME Seconds"
 
