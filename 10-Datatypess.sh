d2bf1d0cc45e321e3c20630bb2f0336e93630278
rajasekhar burri <burriraja@gmail.com>
2025-09-26 22:31:02 +0530

    general coversation

--- 10-Datatypess.sh
+++ 10-Datatypess.sh
@@ -11,10 +11,10 @@ echo "SUM is: ${SUM}"
 
 LEADERS=("modi" "putin" "trudo" "trump")
 
-echo "All leaders: ${LEADERS[@]}"
+echo "All leaders: ${LEADERS[@]}" #print all names
 
-echo "All leaders: ${LEADERS[0]}"
+echo "All leaders: ${LEADERS[0]}" #print first name
 
-echo "All leaders: ${LEADERS[1]} ${LEADERS[2]} ${LEADERS[3]}"
-
-echo "All leaders: ${LEADERS[*]}"
+#echo "All leaders: ${LEADERS[1]} ${LEADERS[2]} ${LEADERS[3]}"
+echo "All leaders: ${LEADERS[2]}  #print second name
+echo "All leaders: ${LEADERS[*]}" #print all names
