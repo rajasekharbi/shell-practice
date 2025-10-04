c278328c7bd5e641b4337fe5050dee838ac5fc10
rajasekhar burri <burriraja@gmail.com>
2025-10-04 15:34:08 +0530

    practice

--- 15-installation.sh
+++ 15-installation.sh
@@ -1,2 +1,27 @@
-#!/bin/bash
+#!/bin/bash\
 
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+   exit 1
+fi
+
+VALIDATE(){
+    if [ $? -ne 0 ]; then
+     echo "ERROR:: Installing $2 is Failure"
+
+     exit 1
+    else 
+    echo "Installing $2 is SUCCESS"
+    fi
+}
+
+dnf install mysql -y
+VALIDATE $? "MYSQL"
+
+dnf install mysql -y
+VALIDATE $? "Nginx"
+
+dnf install mysql -y
+VALIDATE $? "mangosh"
