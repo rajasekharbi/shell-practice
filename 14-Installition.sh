27c7adc8abacfb24f2e4360e44a4b147accb03c5
rajasekhar burri <burriraja@gmail.com>
2025-10-04 15:15:25 +0530

    practice

--- 14-Installition.sh
+++ 14-Installition.sh
@@ -4,7 +4,7 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
     echo "ERROR:: Please run this script with root privelege"
-   
+   exit 1
 fi
 
 dnf install mysql -y
@@ -13,7 +13,7 @@ if [ %? -ne 0 ]; then
 
     echo "ERROR:: Installing My SQL is Failure"
 
-     
+     exit 1
 else 
     echo "Installing MYSQL is SUCCESS"
 fi
