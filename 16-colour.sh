ac76c6a97697de64741e9ce7b1d4aef86abee91f
rajasekhar burri <burriraja@gmail.com>
2025-10-05 15:13:07 +0530

    practice

--- 16-colour.sh
+++ 16-colour.sh
@@ -5,5 +5,5 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-echo -e "$G Hello world $R"
+echo -e "$G Hello world $N"
 echo "check this color"
---
+++ 17-colour installation.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+   exit 1
+fi
+
+VALIDATE(){
+    if [ $? -ne 0 ]; then
+     echo "ERROR:: Installing $2 is Failure"
+
+     exit 1
+    else 
+    echo "Installing $2 is SUCCESS"
+    fi
+}
+
+dnf install mysql -y
+VALIDATE $? "MYSQL"
+
+dnf install nginx -y
+VALIDATE $? "Nginx"
+
+dnf install python3 -y
+VALIDATE $? "python3"
